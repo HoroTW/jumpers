303588ea2f66b90db30878c255f9a88fb24947df
HoroTW <horo@horotw.com>
2022-02-17 13:06:34 +0100

    Improve: Formatting of the graph

--- visualize.py
+++ visualize.py
@@ -4,10 +4,13 @@
 from time import sleep
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
 
 # %%
 REFRESH_IN_SECS = 30
-PATH_FOR_PDF_OUTPUT = "./"
+PATH_FOR_PDF_OUTPUT = "/dockerMounts/caddy/site/jumpers/"
+plt.rcParams["figure.figsize"] = (24,14) 
 
 # %%
 while True:
@@ -16,7 +19,10 @@ while True:
         data: pd.DataFrame = pd.read_pickle("jumpers.pkl.gzip", compression="gzip")
         # data set index to timestamp
         data.set_index("timestamp", inplace=True)
-        data.plot()
+        data.drop(columns=["maxCheckinsAllowed"],  inplace=True)
+        ax = data.plot(x_compat=True) 
+        ax.xaxis.set_major_locator(mdates.HourLocator())               
+        plt.yticks(np.arange(min(data.countCheckedInCustomer), max(data.countCheckedInCustomer)+1, 5))
         plt.savefig(PATH_FOR_PDF_OUTPUT + "jumpers.pdf")
         print("refreshed pdf")
     except Exception as e:
